12db788f10a93a58c1b4dab4b5391d990379e7b1
Abaevsk94 <abaevsk@gmail.com>
2023-01-15 17:19:04 +0300

    Second level

--- src/Employee.java
+++ src/Employee.java
@@ -3,7 +3,7 @@ public class Employee {
     private final String firstName;
     private final String middleName;
     private int department;
-    private int salary;
+    private double salary;
     private static int counter;
     private final int id;
 
@@ -36,11 +36,11 @@ public class Employee {
         this.department = department;
     }
 
-    public int getSalary() {
+    public double getSalary() {
         return this.salary;
     }
 
-    public void setSalary(int salary) {
+    public void setSalary(double salary) {
         this.salary = salary;
     }
 
@@ -48,8 +48,12 @@ public class Employee {
         return id;
     }
 
+    public String getNick() {
+        return getLastName() + " " + getFirstName() + " " + getMiddleName();
+    }
+
     public String toString() {
-        return "ID: " + getId() + " | Ф.И.О. сотрудника: " + getLastName() + " " + getFirstName() + " " + getMiddleName() + " | Отдел: " + getDepartment() + " | Зарплата: " + getSalary();
+        return "ID: " + getId() + " | Ф.И.О. сотрудника: " + getNick() + " | Отдел: " + getDepartment() + " | Зарплата: " + getSalary();
     }
 
     public static void printEmployeeList(Employee[] employees) {
@@ -85,12 +89,110 @@ public class Employee {
         }
         return employeeWithHighestSalary;
     }
+
     public static double calculateAverageSalary(Employee[] employees) {
         return calculateTotalSalary(employees) / employees.length;
     }
-    public static void printEmployees(Employee[] employees) {
+
+    public static void printEmployeesNames(Employee[] employees) {
         for (Employee employee : employees) {
-            System.out.println(employee.getLastName() + " " + employee.getFirstName() + " " + employee.getMiddleName());
+            System.out.println(employee.getNick());
+        }
+    }
+
+    public static void indexSalary(Employee[] employees, double percent) {
+        for (Employee employee : employees) {
+            double newSalary = employee.getSalary() * (1 + percent / 100);
+            employee.setSalary(newSalary);
+        }
+    }
+
+    public static int chooseDepartment(int department) {
+        return department;
+    }
+
+    public static Employee findLowestPaidEmployeeInDepartment(Employee[] employees, int department) {
+        Employee lowestPaidEmployeeInDepartment = null;
+        double lowestSalary = Double.MAX_VALUE;
+        for (Employee employee : employees) {
+            if (employee.getDepartment() == department && employee.getSalary() < lowestSalary) {
+                lowestPaidEmployeeInDepartment = employee;
+                lowestSalary = employee.getSalary();
+                employee.setDepartment(department);
+            }
+        }
+        return lowestPaidEmployeeInDepartment;
+    }
+
+    public static Employee findHighestPaidEmployeeInDepartment(Employee[] employees, int department) {
+        Employee highestPaidEmployeeInDepartment = null;
+        double highestSalary = Double.MIN_VALUE;
+        for (Employee employee : employees) {
+            if (employee.getDepartment() == department && employee.getSalary() > highestSalary) {
+                highestPaidEmployeeInDepartment = employee;
+                highestSalary = employee.getSalary();
+                employee.setDepartment(department);
+            }
+        }
+        return highestPaidEmployeeInDepartment;
+    }
+
+    public static double getTotalSalaryExpenseByDepartment(Employee[] employees, int department) {
+        double totalExpense = 0;
+        for (Employee employee : employees) {
+            if (employee.getDepartment() == department) {
+                totalExpense += employee.getSalary();
+                employee.setDepartment(department);
+            }
+        }
+        return totalExpense;
+    }
+
+    public static double getAverageSalaryByDepartment(Employee[] employees, int department) {
+        double totalSalary = 0;
+        int employeeCount = 0;
+        for (Employee employee : employees) {
+            if (employee.getDepartment() == department) {
+                totalSalary += employee.getSalary();
+                employeeCount++;
+                employee.setDepartment(department);
+            }
+        }
+        return totalSalary / employeeCount;
+    }
+
+    public static void indexSalaryByDepartment(Employee[] employees, int department, double percent) {
+        for (Employee employee : employees) {
+            if (employee.getDepartment() == department) {
+                double newSalary = employee.getSalary() * (1 + percent / 100);
+                employee.setSalary(newSalary);
+                employee.setDepartment(department);
+            }
+        }
+    }
+
+    public static void printEmployeesByDepartment(Employee[] employees, int department) {
+        for (Employee employee : employees) {
+            if (employee.getDepartment() == department) {
+                System.out.println("ID: " + employee.getId() + " | Ф.И.О. сотрудника: " + employee.getNick() + " | Зарплата: " + employee.getSalary());
+            }
+        }
+    }
+
+
+    public static void printEmployeesWithSalaryLessThen(Employee[] employees, int salary) {
+        for (Employee employee : employees) {
+            if (employee.getSalary() < salary) {
+                System.out.println("ID: " + employee.getId() + " | Ф.И.О. сотрудника: " + employee.getNick() + " | Зарплата: " + employee.getSalary());
+            }
+        }
+    }
+
+    public static void printEmployeesWithSalaryMoreThen(Employee[] employees, int salary) {
+        for (Employee employee : employees) {
+            if (employee.getSalary() > salary) {
+                System.out.println("ID: " + employee.getId() + " | Ф.И.О. сотрудника: " + employee.getNick() + " | Зарплата: " + employee.getSalary());
+            }
         }
     }
 }
